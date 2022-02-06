a5fc519c9346f7260672369f471ddb09b50a70c7
Kseniya Smirnova <fanni@bk.ru>
2022-02-06 13:54:01 +0300

    hw1

---
+++ src/p2-homeworks/h1/tests/Avatar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+ type AvatarPropsType = {
+    avatar: string
+}
+
+
+const Avatar= (props: AvatarPropsType) => {
+    return <img src="'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg'," alt="ava" />
+}
+
+
+
+
+
+export default Avatar;
